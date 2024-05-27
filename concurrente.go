3ac472e0050d70196b7a83206dbd756c6fe9b6b0
cesar <cesar6793@gmail.com>
2024-05-26 22:04:07 -0500

    probando resultados

--- concurrente.go
+++ concurrente.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"io/ioutil"
 	"math"
+	"math/rand"
 	"net/http"
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 )
 
 func distance(a, b []float64) float64 {
@@ -19,7 +21,7 @@ func distance(a, b []float64) float64 {
 	return math.Sqrt(sum)
 }
 
-func closestCentroid(point []float64, centroids [][]float64, idx int, assignments chan<- int, wg *sync.WaitGroup) {
+func closestCentroid(point []float64, centroids [][]float64, assignments chan<- int, wg *sync.WaitGroup) {
 	defer wg.Done()
 	minDist := math.MaxFloat64
 	var closestIdx int
@@ -59,6 +61,66 @@ func updateCentroids(points [][]float64, assignments []int, k int, newCentroids 
 	}
 
 	newCentroids <- centroids
+}
+
+func calculateCost(points [][]float64, centroids [][]float64, assignments []int) float64 {
+	var totalCost float64
+	for i, point := range points {
+		centroid := centroids[assignments[i]]
+		totalCost += distance(point, centroid)
+	}
+	return totalCost
+}
+
+func kMeans(points [][]float64, k int, maxIterations int) ([][]float64, []int, float64) {
+	rand.Seed(time.Now().UnixNano())
+
+	centroids := make([][]float64, k)
+	for i := range centroids {
+		centroids[i] = points[rand.Intn(len(points))]
+	}
+
+	assignments := make([]int, len(points))
+
+	for iter := 0; iter < maxIterations; iter++ {
+		var wg sync.WaitGroup
+		assignmentsChan := make(chan int, len(points))
+		newCentroidsChan := make(chan [][]float64, 1)
+
+		wg.Add(len(points))
+		for _, point := range points {
+			go closestCentroid(point, centroids, assignmentsChan, &wg)
+		}
+		wg.Wait()
+		close(assignmentsChan)
+
+		i := 0
+		for assign := range assignmentsChan {
+			assignments[i] = assign
+			i++
+		}
+
+		wg.Add(1)
+		go updateCentroids(points, assignments, k, newCentroidsChan, &wg)
+		wg.Wait()
+		newCentroids := <-newCentroidsChan
+
+		converged := true
+		for i := range centroids {
+			if distance(centroids[i], newCentroids[i]) > 0.001 {
+				converged = false
+				break
+			}
+		}
+		if converged {
+			break
+		}
+
+		centroids = newCentroids
+	}
+
+	cost := calculateCost(points, centroids, assignments)
+	return centroids, assignments, cost
 }
 
 func main() {
@@ -101,56 +163,29 @@ func main() {
 	}
 
 	k := 4
-
-	centroids := make([][]float64, k)
-	copy(centroids, points[:k])
-
-	assignments := make([]int, len(points))
 	maxIterations := 100
+	bestCentroids := [][]float64{}
+	bestAssignments := []int{}
+	bestCost := math.MaxFloat64
 
-	for iter := 0; iter < maxIterations; iter++ {
-		var wg sync.WaitGroup
-		assignmentsChan := make(chan int, len(points))
-		newCentroidsChan := make(chan [][]float64, 1)
+	numRuns := 1000
 
-		wg.Add(len(points))
-		for i, point := range points {
-			go closestCentroid(point, centroids, i, assignmentsChan, &wg)
+	for run := 0; run < numRuns; run++ {
+		centroids, assignments, cost := kMeans(points, k, maxIterations)
+		if cost < bestCost {
+			bestCost = cost
+			bestCentroids = centroids
+			bestAssignments = assignments
 		}
-		wg.Wait()
-		close(assignmentsChan)
-
-		i := 0
-		for assign := range assignmentsChan {
-			assignments[i] = assign
-			i++
-		}
-
-		wg.Add(1)
-		go updateCentroids(points, assignments, k, newCentroidsChan, &wg)
-		wg.Wait()
-		newCentroids := <-newCentroidsChan
-
-		// Verificar convergencia
-		converged := true
-		for i := range centroids {
-			if distance(centroids[i], newCentroids[i]) > 0.001 {
-				converged = false
-				break
-			}
-		}
-		if converged {
-			break
-		}
-
-		centroids = newCentroids
+		fmt.Printf("Run %d, Cost: %.4f\n", run+1, cost)
 	}
 
-	fmt.Println("Centroides:")
-	for _, centroid := range centroids {
+	fmt.Println("Mejores centroides encontrados:")
+	for _, centroid := range bestCentroids {
 		fmt.Println(centroid)
 	}
 
-	fmt.Println("Asignaciones de puntos:")
-	fmt.Println(assignments)
+	fmt.Println("Mejores asignaciones de puntos:")
+	fmt.Println(bestAssignments)
+	fmt.Printf("Mejor costo: %.4f\n", bestCost)
 }
